3edac437bd33658be967d30c3bd088d010f0be09
Talha Ghauri <talhaghauridev@gmail.com>
2024-05-18 11:08:53 -0700

    add NODE_ENV

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,7 +5,8 @@ configDotenv({
 });
 
 const DATABASE_URL = process.env.DATABASE_URL!;
+const NODE_ENV = process.env.NODE_ENV!;
 
 const PORT = Number(process.env.PORT || 4000);
 
-export { DATABASE_URL, PORT };
+export { DATABASE_URL, PORT, NODE_ENV };
